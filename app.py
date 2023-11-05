e41df95415cc9fe6d85c50046bd70b82819d8c16
Pranav904 <pranavnv14@gmail.com>
2023-11-05 20:40:33 +0530

    Changes to be committed: 	modified:   app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os
-from time import sleep
-import threading
+# from time import sleep
+# import threading
 from icg import *
 
 app = Flask(__name__)
@@ -11,9 +11,9 @@ upload_folder = 'static\\upload'
 if not os.path.exists(upload_folder):
     os.makedirs(upload_folder)
 
-def del_file(path):
-    sleep(15)
-    os.remove(path)
+# def del_file(path):
+#     sleep(15)
+#     os.remove(path)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -27,9 +27,9 @@ def upload_file():
         file.save(img)
         caption = predict_caption(caption_model, tokenizer, max_length, feature_extract(img))
         render = render_template('index.html',vimg = True, img=img, cap = caption[18:-7])
-        # Start a new thread to delete the file after 15 seconds
-        deletion_thread = threading.Thread(target=del_file, args=(img,))
-        deletion_thread.start()
+        # # Start a new thread to delete the file after 15 seconds
+        # deletion_thread = threading.Thread(target=del_file, args=(img,))
+        # deletion_thread.start()
         return render
     return render_template('index.html')
 
